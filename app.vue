1f17a6da4afc3226165b4f4f65a0919dbe17c6e5
DevShubrato <shubratokumargharami920@gmail.com>
2023-09-23 16:43:20 +0600

    tailwind and config

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      "Hi this is Shubrato, Frontend Developer at GetBoadred Technologies Ltd"
+    </h1>
   </div>
 </template>
