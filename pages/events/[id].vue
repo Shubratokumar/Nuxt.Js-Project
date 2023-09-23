d5300b7a074d4504d514b8a9c0060347fb3e82d9
DevShubrato <shubratokumargharami920@gmail.com>
2023-09-23 17:39:07 +0600

    dynamic route in nuxt and useRoute hook

---
+++ pages/events/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center">
+    <h1 class="text-2xl font-bold">
+        Dynamic id: {{ this.$route.params.id }}
+    </h1>
+  </div>
+</template>
+<script setup>
+    const route = useRoute();
+    console.log(route.params.id);    
+</script>
