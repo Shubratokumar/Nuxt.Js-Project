31558a3d0a913bf68a62dbc71e6bed32d72ecdf7
DevShubrato <shubratokumargharami920@gmail.com>
2023-09-24 00:01:19 +0600

    components and auto import

---
+++ components/Alert.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-green-500 rounded p-2 text-white m-10">
+        <h3>
+            This is an alert component.            
+        </h3>
+    </div>
+</template>
+<script setup>
+</script>
+<style lang="scss">
+    
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,7 @@
     <h1 class="text-3xl font-bold underline">
       "Hi this is Shubrato, Frontend Developer at GetBoadred Technologies Ltd"
     </h1>
+    <!-- Importing components -->
+    <Alert/>
   </div>
 </template>
