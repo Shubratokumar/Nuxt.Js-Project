e9d191ad57c7dd49e0cd9d19b7c44a25cdff3c64
DevShubrato <shubratokumargharami920@gmail.com>
2023-10-08 13:54:04 +0600

    layouts in nuxt

---
+++ layouts/custom.vue
@@ -0,0 +1,8 @@
+<template lang="">
+  <div class="CustomLayout bg-cyan-400">
+    <p>Custom layout is here ...</p>
+    <slot />
+  </div>
+</template>
+<style lang="scss">
+</style>
---
+++ pages/custom.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Custom pages here ...!!!</h1>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  layout: "custom",
+});
+</script>
