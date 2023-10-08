f31c800a1af1fc3dcea9837a3204619076bbf496
DevShubrato <shubratokumargharami920@gmail.com>
2023-10-08 17:16:39 +0600

    assets in nuxt

--- pages/custom.vue
+++ pages/custom.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="container">
     <h1 class="text-3xl font-bold underline">Custom pages here ...!!!</h1>
+    <img class="w-full h-full" src="@/assets/2.png" alt="Discover Nuxt 3" />
   </div>
 </template>
 <script setup>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container h-screen">
     <h1 class="text-3xl font-bold underline">
       "Hi this is Shubrato, Frontend Developer at GetBoadred Technologies Ltd"
     </h1>
@@ -8,3 +8,10 @@
     <Profile/>
   </div>
 </template>
+<style lang="scss">
+.container{
+  background-image: url(../assets/1.png);
+  background-repeat: repeat;
+  background-size: cover;
+}
+</style>
